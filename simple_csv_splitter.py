c8e68c1d87ace0db1113f0f53e8a2275790ffd2f
ryelye <oliverxhu@hotmail.com>
2020-10-29 22:57:26 +1300

    simple csv splitter

--- simple_csv_splitter.py
+++ simple_csv_splitter.py
@@ -3,13 +3,20 @@ import os
 import math
 import argparse
 
+# defaults
+input_header = 0
+output_header = 1
+delimiter = ","
+row_limit = 100000
+
 # add CLI arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('input', metavar='i', type=str, help='input file')
 parser.add_argument('output', metavar='o', type=str, help='output path (with no filename)')
 parser.add_argument('-d', '--delimiter', help='default comma ","')
 parser.add_argument('-r', '--row_limit', help='max row per CSV. Default 100k')
-parser.add_argument('-nh', '--no_header', help='set this to 1 to not process headers')
+parser.add_argument('-ih', '--input_header', help="0 or 1. If the input has headers or not. Default = 1")
+parser.add_argument('-oh', '--output_header', help='0 or 1. To output headers or not. Default = 1')
 
 args = vars(parser.parse_args())
 
@@ -18,29 +25,35 @@ input_file = args['input']
 output_path = args['output']
 if args['delimiter']:
     delimiter = args['delimiter']
-else:
-    delimiter = ","
 if args['row_limit']:
-    row_limit = args['row_limit']
-else:
-    row_limit = 100000
-row_limit = int(row_limit)
-if args['no_header']=="1":
-    no_header = True
-else:
-    no_header = False
+    row_limit = int(args['row_limit'])
+if args['input_header'] == "1":
+    input_header = 0
+elif args['input_header'] == "0":
+    input_header = None
+if args['output_header'] == "1":
+    output_header = True
+elif args['output_header'] == "0":
+    output_header = False
 
 # rename output path
 output_name_template = f"{os.path.split(input_file)[1].split('.')[0]}_output%s.csv"
 output_path_full = os.path.join(output_path, output_name_template)
 
 # read and split the file
-df = pd.read_csv(input_file, delimiter=delimiter)
+df = pd.read_csv(input_file, delimiter=delimiter, header=input_header)
 chunks = math.ceil(len(df) / row_limit)
 dt = {}
 for i in range(chunks):
     dt[i] = [row_limit * i, row_limit * (i+1)]
-for i in range(chunks):
-    df[dt[i][0]:dt[i][1]].to_csv(output_name_template % i, header=not no_header, index=False)
 
-print('Split complete')
+# if input has no header, then don't output a header too
+# if input_header == 0:
+for i in range(chunks):
+    df[dt[i][0]:dt[i][1]].to_csv(output_path_full % i, header=output_header if input_header == 0 else False, index=False)
+# if there is no input header, there is no output header too.
+# elif input_header is None:
+#     for i in range(chunks):
+#         df[dt[i][0]:dt[i][1]].to_csv(output_name_template % i, header=False, index=False)
+
+print('Split complete. Outputted to file: %s' % output_name_template % '', 'Total files = %s' % chunks)
